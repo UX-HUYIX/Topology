63b3cb9b181a2da2c7f7fa167b2aefa948994ce6
UX-HUYIX <mr.veapetyan@gmail.com>
2026-04-21 20:05:22 +0300

    Add KenopsisInterpreter class with interpretation methods

---
+++ backend/interpreter.py
@@ -0,0 +1,8 @@
+class KenopsisInterpreter:
+    def generate_interpretation(self):
+        # Implementation for generating interpretation
+        pass
+
+    def generate_modulations_only(self):
+        # Implementation for generating modulations only
+        pass
